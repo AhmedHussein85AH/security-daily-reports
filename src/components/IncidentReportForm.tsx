dcb989dbfae8d55f04cb30f6f26aff7655a8666a
Ahmed_Hussein <AhmedHusseinElsayed@outlook.com>
2025-10-17 14:28:05 +0300

    removed unused default React

--- src/components/IncidentReportForm.tsx
+++ src/components/IncidentReportForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { IncidentData, ResponsibilityEntry } from '../types/incident'
 // Icons will be defined as inline SVGs
 
